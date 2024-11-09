a882b49c01827eb56676c96422fe8029fe9e2cca
JuliyaG13 <julia.gumirova.gm@gmail.com>
2024-11-09 20:39:58 +0300

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,27 +1,24 @@
-let totalSeconds = 300; 
+let totalSeconds = 59;
 
-function startTimer() {
+function updateTimer() {
     const timerElement = document.getElementById('timer');
 
-    const interval = setInterval(() => {
-    
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
-        const formattedTime = String(hours).padStart(2, '0') + ':' + 
-             String(minutes).padStart(2, '0') + ':' + 
-             String(seconds).padStart(2, '0');
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
 
-       
-        timerElement.textContent = formattedTime;
+    const formattedTime = String(hours).padStart(2, '0') + ':' +
+                          String(minutes).padStart(2, '0') + ':' +
+                          String(seconds).padStart(2, '0');
 
-        totalSeconds--;
+    timerElement.textContent = formattedTime;
 
-        if (totalSeconds < 0) {
-            clearInterval(interval);
-            alert('Вы победили в конкурсе!');
-        }
-    }, 1000);
+    totalSeconds--;
+
+    if (totalSeconds < 0) {
+        clearInterval(interval);
+        alert("Вы победили в конкурсе!");
+    }
 }
 
-window.onload = startTimer;
+const interval = setInterval(updateTimer, 1000);
